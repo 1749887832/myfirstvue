bdb3c19d971aab3214a50f1b1d4d28014a38c443
liu <1749887832@qq.com>
2021-01-14 20:47:44 +0800

    localStorage

--- src/router.js
+++ src/router.js
@@ -36,7 +36,7 @@ router.beforeEach((to, from, next) => {
         return next()
     } else {
         // 清除token
-        const tokenStr = window.sessionStorage.getItem('token')
+        const tokenStr = window.localStorage.getItem('token')
         if (!tokenStr) {
             return next('/login')
         } else {
